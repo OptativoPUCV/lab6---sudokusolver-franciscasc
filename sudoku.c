04fe56092f4e6c77ba8f951f45fc13ae2ea6ee85
Replit user <>
2023-05-06 21:50:01 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -167,7 +167,7 @@ Node* DFS(Node* initial, int* cont){
 
     List *listaAdy = get_adj_nodes(nodo);
     Node *nodoAux = first(listaAdy);
-    if(nodoAux != NULL){ 
+    if(!is_valid(nodoAux)){ 
       push(S, nodoAux);
       nodoAux = next(listaAdy);
     } 
