6cac060bedc53f0e20268513d420373c7e85ea66
Replit user <>
2023-05-06 21:24:11 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -164,7 +164,7 @@ Node* DFS(Node* initial, int* cont){
     Node *nodoAux = first(listaAdy);
    
     if(nodoAux != NULL){ 
-      if(is_valid(nodoAux)){
+      if(!is_valid(nodoAux)){
         push(S, nodoAux);
         nodoAux = next(listaAdy);
       } 
