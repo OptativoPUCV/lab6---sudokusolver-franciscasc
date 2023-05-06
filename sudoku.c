7532c9a926bb06e7f1b6ff82d96052bd7ed69200
Replit user <>
2023-05-06 21:16:24 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -154,6 +154,7 @@ Node* DFS(Node* initial, int* cont){
   push(S, initial);
   
   while(!is_empty(S)){
+    printf("h\n");
     Node * nodo = first(S);
     pop(S);
     if(is_final(nodo)) 
@@ -161,6 +162,7 @@ Node* DFS(Node* initial, int* cont){
 
     List *listaAdy = get_adj_nodes(nodo);
     Node *nodoAux = first(listaAdy);
+    printf("h\n");
     if(nodoAux != NULL){
       push(S, nodoAux);
       nodoAux = next(listaAdy);
